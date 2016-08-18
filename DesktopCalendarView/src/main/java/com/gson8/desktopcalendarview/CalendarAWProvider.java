2a9f199a2420adca0ea65cb456cf158c391248aa
Syusuke <Zyj7810@126.com>
2016-08-18 23:35:45 +0800

    init

---
+++ DesktopCalendarView/src/main/java/com/gson8/desktopcalendarview/CalendarAWProvider.java
@@ -0,0 +1,88 @@
+package com.gson8.desktopcalendarview;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.util.Log;
+import android.widget.RemoteViews;
+import android.widget.Toast;
+
+public class CalendarAWProvider extends AppWidgetProvider {
+
+    private static final String TAG = CalendarAWProvider.class.getSimpleName();
+
+    public static final String ACTION_LUNAR = "com.gson8.desktopcalendarview.LUNAR_CLICK";
+    public static final String ACTION_GRIDVIEW_ITEM = "com.gson8.desktopcalendarview.GRIDVIEW_ITEM";
+
+    public static final String EXTRA_ITEM_FULL_LUNAR =
+            "com.gson8.desktopcalendarview.EXTRA_ITEM_FULL_LUNAR";
+    public static final String EXTRA_ITEM_MONTH = "com.gson8.desktopcalendarview.EXTRA_ITEM_MONTH";
+    public static final String EXTRA_ITEM_YEAR = "com.gson8.desktopcalendarview.EXTRA_ITEM_YEAR";
+    public static final String EXTRA_ITEM_DAY = "com.gson8.desktopcalendarview.EXTRA_ITEM_DAY";
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+                         int[] appWidgetIds) {
+
+        for(int appWidgetId : appWidgetIds) {
+            RemoteViews rv = new RemoteViews(context.getPackageName(), R.layout.layout_calendar);
+
+            //最上面农历的点击事件
+            Intent btIntent = new Intent().setAction(ACTION_LUNAR);
+            PendingIntent btPendingIntent = PendingIntent
+                    .getBroadcast(context, 0, btIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+            rv.setOnClickPendingIntent(R.id.tv_title_lunar, btPendingIntent);
+
+            //GridView点击事件
+            Intent serviceIntent = new Intent(context, CalendarAWService.class);
+            rv.setRemoteAdapter(R.id.gridview, serviceIntent);
+
+            Intent gridIntent = new Intent();
+            gridIntent.setAction(ACTION_GRIDVIEW_ITEM);
+            gridIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+            PendingIntent pendingIntent = PendingIntent
+                    .getBroadcast(context, 0, gridIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+            rv.setPendingIntentTemplate(R.id.gridview, pendingIntent);
+            appWidgetManager.updateAppWidget(appWidgetId, rv);
+        }
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+        Log.e(TAG, "onReceive : " + intent.getAction());
+
+        if(action.equals(ACTION_GRIDVIEW_ITEM)) {
+            int appWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,
+                    AppWidgetManager.INVALID_APPWIDGET_ID);
+            String fullLunar = intent.getStringExtra(EXTRA_ITEM_FULL_LUNAR);
+
+            int day = intent.getIntExtra(EXTRA_ITEM_DAY, 1);
+            int month = intent.getIntExtra(EXTRA_ITEM_MONTH, 1);
+            int year = intent.getIntExtra(EXTRA_ITEM_YEAR, 1900);
+
+            RemoteViews remoteViews =
+                    new RemoteViews(context.getPackageName(), R.layout.layout_calendar);
+            remoteViews.setTextViewText(R.id.tv_title_lunar, fullLunar);
+
+            remoteViews.setTextColor(R.id.tv_gvitem_lunar, Color.RED);
+            remoteViews.setTextColor(R.id.tv_gvitem_date, Color.RED);
+
+
+//            Intent serviceIntent = new Intent(context, CalendarAWService.class);
+//            remoteViews.setRemoteAdapter(R.id.gridview, serviceIntent);
+
+            appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
+
+        } else if(action.equals(ACTION_LUNAR)) {
+            Toast.makeText(context, "Lunar Click", Toast.LENGTH_SHORT).show();
+        }
+        super.onReceive(context, intent);
+    }
+}
---
+++ DesktopCalendarView/src/main/java/com/gson8/desktopcalendarview/CalendarAWService.java
@@ -0,0 +1,135 @@
+package com.gson8.desktopcalendarview;
+
+import android.appwidget.AppWidgetManager;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CalendarAWService extends RemoteViewsService {
+
+    private static final String TAG = CalendarAWService.class.getSimpleName();
+
+    @Override
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+        return new CalendarAWRemoteViewsFactory(this, intent);
+    }
+
+    private class CalendarAWRemoteViewsFactory implements RemoteViewsFactory {
+
+        private Context mContext;
+        private int mAppWidgetId;
+
+        private String IMAGE_ITEM = "imgage_item";
+        private String TEXT_ITEM = "text_item";
+        private List<DateBean> mDateData;
+
+        public CalendarAWRemoteViewsFactory(Context context, Intent intent) {
+            mContext = context;
+            mAppWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,
+                    AppWidgetManager.INVALID_APPWIDGET_ID);
+        }
+
+        @Override
+        public RemoteViews getViewAt(int position) {
+            DateBean bean = mDateData.get(position);
+
+            // 获取 item_calendarxml 对应的RemoteViews
+            RemoteViews rv = new RemoteViews(mContext.getPackageName(), R.layout.item_calendar);
+
+            if(bean != null) {
+                rv.setTextViewText(R.id.tv_gvitem_date, mDateData.get(position).getDay() + "");
+                rv.setTextViewText(R.id.tv_gvitem_lunar, mDateData.get(position).getShowLunarDay());
+
+                Intent fillInIntent = new Intent();
+                fillInIntent
+                        .putExtra(CalendarAWProvider.EXTRA_ITEM_FULL_LUNAR, bean.getFullLunar());
+                fillInIntent.putExtra(CalendarAWProvider.EXTRA_ITEM_DAY, bean.getDay());
+                fillInIntent.putExtra(CalendarAWProvider.EXTRA_ITEM_MONTH, bean.getYear());
+                fillInIntent.putExtra(CalendarAWProvider.EXTRA_ITEM_YEAR, bean.getMonth());
+
+                int it = DateUtils.isToday(bean);
+                if(it != -1 && it == position) {
+                    rv.setTextColor(R.id.tv_gvitem_date, Color.RED);
+                    rv.setTextColor(R.id.tv_gvitem_lunar, Color.RED);
+                }
+                rv.setOnClickFillInIntent(R.id.item_layout, fillInIntent);
+            } else {
+                rv.setTextViewText(R.id.tv_gvitem_date, "");
+                rv.setTextViewText(R.id.tv_gvitem_lunar, "");
+            }
+            return rv;
+        }
+
+        @Override
+        public void onCreate() {
+            mDateData = new ArrayList<>();
+
+            update(2016, 8);
+        }
+
+        public void update(int year, int month) {
+            mDateData.clear();
+            int aMonthNum = DateUtils.getDaysByYearMonth(year, month);
+            int weekNum = DateUtils.getWeek(year, month);
+
+            int index = 0;
+
+            for(int i = 0; i < aMonthNum + weekNum - 1; i++) {
+                if(index < weekNum - 1) {
+                    mDateData.add(null);
+                    index++;
+                } else {
+                    int day = i - weekNum + 2;
+                    mDateData.add(new DateBean(year, month, day));
+                }
+            }
+        }
+
+
+        @Override
+        public int getCount() {
+            // 返回“集合视图”中的数据的总数
+            return mDateData == null ? 0 : mDateData.size();
+        }
+
+        @Override
+        public long getItemId(int position) {
+            // 返回当前项在“集合视图”中的位置
+            return position;
+        }
+
+        @Override
+        public RemoteViews getLoadingView() {
+//            return new RemoteViews(mContext.getPackageName(), R.layout.item_loading);
+            return null;
+        }
+
+        @Override
+        public int getViewTypeCount() {
+            // 只有一类 GridView
+            return 1;
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
+        @Override
+        public void onDataSetChanged() {
+
+        }
+
+        @Override
+        public void onDestroy() {
+            mDateData.clear();
+        }
+
+
+    }
+}
